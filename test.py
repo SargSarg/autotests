3d602f02c71e75dddbe018cdce8c698e9588e3cd
SargSarg <149015139+SargSarg@users.noreply.github.com>
2024-02-08 02:46:22 +0400

    Update test.py

--- test.py
+++ test.py
@@ -9,3 +9,13 @@ class TestCalc:
 
     def test_adding_success(self):
         assert self.calc.adding(self, 1, 1) == 2
+        
+    def test_adding_unsuccess(self):
+        assert self.calc.adding(self, 1, 1) == 3
+
+    def test_zero_division(self):
+        with pytest.raises(ZeroDivisionError):
+            self.calc.division(self, 1, 0)
+
+    def teardown(self):
+        print('Выполнение метода Teardown')
